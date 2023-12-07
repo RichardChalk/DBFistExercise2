b672566f44499241367905b65e484e33ab4d2558
Richard Chalk <barmybrit@gmail.com>
2023-12-07 06:43:07 +0100

    Added comment about lazy loading

--- Model/Student.cs
+++ Model/Student.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Runtime.CompilerServices;
 
 #nullable disable
 
@@ -12,6 +14,16 @@ namespace DBFistExercise2.Model
         public string Efternamn { get; set; }
         public DateTime? Birthdate { get; set; }
         public int? KursId { get; set; }
+
+        // Entity Framework och Lazy Loading:
+        // Ett vanligt skäl till att använda virtual i detta sammanhang
+        // är relaterat till Entity Framework, ett ORM(Object-Relational Mapping) verktyg i.NET.
+        
+        // När en egenskap är märkt som virtual, kan Entity Framework använda lazy loading.
+        // Det innebär att när du hämtar en Student-entitet från databasen,
+        // laddas inte Kurs-egenskapen direkt.
+        // Istället laddas den först när du faktiskt använder den.
+        // Detta kan förbättra prestanda genom att undvika att ladda onödig data.
         public virtual Kur Kurs { get; set; }
     }
 }
